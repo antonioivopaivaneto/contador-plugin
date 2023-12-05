93378664372ccfd679619475dcd0024b013fa834
junior <antonioivo.3@gmail.com>
2023-12-05 15:48:59 -0300

    ajustment

--- contador-plugin.js
+++ contador-plugin.js
@@ -1,34 +1,32 @@
-const ContadorPlugin = {
-    install(Vue) {
-      Vue.component('Contador', {
-        props: {
-          from: {
-            type: Number,
-            default: 0
-          },
-          to: {
-            type: Number,
-            required: true
-          }
-        },
-        data() {
-          return {
-            count: this.from
-          };
-        },
-        mounted() {
-          const increment = () => {
-            if (this.count < this.to) {
-              this.count++;
-              setTimeout(increment, 1000); // Incremento a cada segundo (1000ms)
-            }
-          };
-          increment();
-        },
-        template: '<div>{{ count }}</div>'
-      });
+// contador-plugin.js
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Contador',
+  props: {
+    from: {
+      type: Number,
+      default: 0
+    },
+    to: {
+      type: Number,
+      required: true
     }
-  };
-  
-  export default ContadorPlugin;
-  
+  },
+  data() {
+    return {
+      count: this.from
+    };
+  },
+  mounted() {
+    const increment = () => {
+      if (this.count < this.to) {
+        this.count++;
+        setTimeout(increment, 1000); // Incremento a cada segundo (1000ms)
+      }
+    };
+    increment();
+  },
+  template: '<div>{{ count }}</div>'
+});
