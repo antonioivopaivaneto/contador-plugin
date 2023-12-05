cea94b0cbf007a5fed6d91c66a3f0bea47976555
junior <antonioivo.3@gmail.com>
2023-12-05 16:55:29 -0300

    teste

--- contador-plugin.js
+++ contador-plugin.js
@@ -1,13 +1,5 @@
-// plugins/i18n.js
 export default {
   install: (app, options) => {
-    // inject a globally available $translate() method
-    app.config.globalProperties.$translate = (key) => {
-      // retrieve a nested property in `options`
-      // using `key` as the path
-      return key.split('.').reduce((o, i) => {
-        if (o) return o[i]
-      }, options)
-    }
-  }
-}
+    alert('teste')
+  },
+};
