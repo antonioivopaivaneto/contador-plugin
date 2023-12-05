060cf1a9bf332098b9bf473fd13c128ba9d57eec
junior <antonioivo.3@gmail.com>
2023-12-05 17:02:56 -0300

    teste3

--- contador-plugin.js
+++ contador-plugin.js
@@ -1,7 +1,22 @@
-import MyComponent from './MyComponent.vue';
-
 export default {
   install: (app, options) => {
-    app.component('MyPluginComponent', MyComponent);
+    app.component('MyPluginComponent', {
+      props: {
+        number1: {
+          type: Number,
+          required: true,
+        },
+        number2: {
+          type: Number,
+          required: true,
+        },
+      },
+      template: `
+        <div>
+          <h1>My Plugin</h1>
+          <p>Result: {{ number1 + number2 }}</p>
+        </div>
+      `,
+    });
   },
 };
